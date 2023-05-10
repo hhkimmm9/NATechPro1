50ca98caa333f389d500fef73e86c3261880e71e
Harrison Kim <hsookim90@gmail.com>
2023-05-09 21:48:17 -0400

    Layout with a sample landing page

---
+++ app/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='border-2 border-black p-1 whitespace-nowrap'>
+      Logo
+    </div>
+  )
+}
+
+export default Logo

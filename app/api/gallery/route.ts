68c337d594eff3a64e7936404a2443a6d3997644
JiwooL0920 <JiwooL0920@gmail.com>
2023-05-10 17:17:16 -0400

    connected db, basic backend filestructure setup

--- app/api/gallery/route.ts
+++ app/api/gallery/route.ts
@@ -1,9 +1,4 @@
-import Gallery from "../../models/GalleryModel";
 import connectMongoDB from "../../lib/db"
-import { NextApiRequest, NextApiResponse } from "next";
-interface RequestBody {
-  name: string;
-}
 
 export async function GET() {
   console.log("connecting")

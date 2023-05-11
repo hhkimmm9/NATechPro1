90e78d4a6f3c4a3e7d7f54de42dda2c070813070
JiwooL0920 <JiwooL0920@gmail.com>
2023-05-10 21:26:37 -0400

    implemented PATCH and DELETE for /api/gallery/[id]

--- app/api/gallery/[id]/route.ts
+++ app/api/gallery/[id]/route.ts
@@ -1,15 +1,41 @@
 import Gallery from "@/models/GalleryModel";
 import { connectMongoDB } from "@/lib/db";
 
+
 export const GET = async (req: Request, { params }: { params: { id: string } } ) => {
     try {
         await connectMongoDB(); 
-
         const gallery = await Gallery.findById(params.id)
-        if (!gallery) return new Response("Gallery not found", { status: 404 });
+        if (!gallery) return new Response(`Gallery id:${params.id} not found`, { status: 404 });
         return new Response(JSON.stringify(gallery), { status: 200 })
 
     } catch (error: any) {
+        console.log(error)
         return new Response("Internal Server Error", { status: 500 });
+    }
+}
+
+export const PATCH = async (req:Request, { params }: { params: { id: string } } ) => {
+    const { name } = await req.json();
+    try {
+        await connectMongoDB();
+        const existingGallery = await Gallery.findById(params.id);
+        if (!existingGallery) return new Response(`Gallery id:${params.id} not found`, { status: 404 });
+        // update
+        existingGallery.name = name; 
+        await existingGallery.save();
+        return new Response(`Successfully updated gallery id:${params.id}`);
+    } catch (err) {
+        return new Response("Error updating gallery", { status: 500 });
+    }
+}
+
+export const DELETE = async (req:Request, { params }: { params: { id: string } } ) => {
+    try {
+        await connectMongoDB();
+        await Gallery.findByIdAndRemove(params.id);
+        return new Response(`Successfully deleted gallery id:${params.id}`);
+    } catch (err) {
+        return new Response("Error updating gallery", { status: 500 });
     }
 }

2bc65e4f653811e1c9146db587a37c84e6958690
JiwooL0920 <JiwooL0920@gmail.com>
2023-05-11 16:31:44 -0400

    cloudinary img upload test works in FE

--- app/backend/utils/cloudinary.js
+++ app/backend/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import cloudinary from 'cloudinary'
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_NAME,
+    api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
 });
 
 const uploads = (file, folder) => {
--- app/test/page.js
+++ app/test/page.js
@@ -1,58 +1,37 @@
 'use client'
 
 import React, { useState } from 'react'
-
+import axios from 'axios'
 
 export default function Test() {
     const [name, setName] = useState("default")
-    const [image, setImage] = useState("/images/default.png")
+    const [image, setImage] = useState("")
+    const cloudinary_name = process.env.NEXT_PUBLIC_CLOUDINARY_NAME
+    const cloudinary_api_key = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY
+    const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_PRESET
 
-    const submitHandler = (e) => {
-        e.preventDefault() 
+
+    async function handleFile(e) {
+        const url = `https://api.cloudinary.com/v1_1/${cloudinary_name}/upload`
         const formData = new FormData()
-        formData.set('name', name)
-        formData.set('image', image)
+        formData.append('file', e.target.files[0])
+        formData.append('api_key', cloudinary_api_key)
+        formData.append('upload_preset', uploadPreset)
+
+        axios.post(url, formData)
+        .then(res => {
+            console.log(res.data.secure_url)
+            setImage(res.data.secure_url)
+        })
+        .catch(err => console.log(err));
 
     }
 
-    const onChange = (e) => {
-        const reader = new FileReader()
-        reader.onload = () => {
-            if (reader.readyState === 2) {
-                setImage(reader.result)
-            }
-        }
-
-        setImage(e.target.files[0])
-        reader.readAsDataURL(e.target.files[0]);
-    }
 
     return (
         <div>
-            <h1>Create a new gallery</h1>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <label>Gallery Name</label>
-                    <input
-                        type="text"
-                        placeholder="Type gallery name"
-                        required
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <div>Upload Image</div>
-                    <img src={image} style={{maxWidth: '500px', maxHeight:'200px'}}/>
-                    <input
-                        type="file"
-                        id="formFile"
-                        onChange={onChange}
-                    />
-
-                </div>
-                <button type="submit">Submit</button>
-            </form>
+            <input type="file" name="image" onChange={handleFile}/>
+            <img src={image} style={{maxWidth: '500px', maxHeight:'200px'}}/>
 
         </div>
     )

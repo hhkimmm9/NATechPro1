31e78c1c7d6c24ea6bda0a1c9b8ac2e2da923f1a
JiwooL0920 <JiwooL0920@gmail.com>
2023-05-21 00:28:50 -0400

    basic commenting for gallery api

--- app/api/gallery/route.ts
+++ app/api/gallery/route.ts
@@ -1,10 +1,18 @@
 import Gallery, { IGallery } from "@/models/GalleryModel"
 import { connectMongoDB } from "@/config/db"
-import upload from "@/backend/utils/multer"
 
-
+/* [GET] http:/localhost:3000/api/gallery  or  http:/localhost:3000/api/gallery?tags=apple,banana
+ *  get all galleries                      or  get all galleries that have tags apple or banana
+ */
 export const GET = async (req:Request) => {
     await connectMongoDB();
+
+    // const accessToken = req.headers.get("authorization");
+    // const token - accessToken?.split(' ')[1];
+    // const decodedToken = verifyJwtToken(token);
+    // if (!accessToken || !decodedToken) {
+        // return new Response("Unauthorized (wrong or expired token", {status:403});
+    // }
 
     try {
         const url = new URL(req.url);
@@ -19,16 +27,12 @@ export const GET = async (req:Request) => {
     }
 }
 
-
+/* [POST] http:/localhost:3000/api/gallery
+ *  Create a gallery 
+ *  req body: name, image, userID, tags
+ */
 export const POST = async (req: Request) => {
     await connectMongoDB();
-
-    // const accessToken = req.headers.get("authorization");
-    // const token - accessToken?.split(' ')[1];
-    // const decodedToken = verifyJwtToken(token);
-    // if (!accessToken || !decodedToken) {
-        // return new Response("Unauthorized (wrong or expired token", {status:403});
-    // }
 
     try {
         const body: IGallery = await req.json();

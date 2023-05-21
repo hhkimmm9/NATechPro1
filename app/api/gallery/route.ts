b591ed198e28cde72547b6d403a94fb9bdf9d2f6
JiwooL0920 <JiwooL0920@gmail.com>
2023-05-21 00:48:27 -0400

    add userID query param to gallery GET req

--- app/api/gallery/route.ts
+++ app/api/gallery/route.ts
@@ -1,8 +1,11 @@
 import Gallery, { IGallery } from "@/models/GalleryModel"
 import { connectMongoDB } from "@/config/db"
 
-/* [GET] http:/localhost:3000/api/gallery  or  http:/localhost:3000/api/gallery?tags=apple,banana
- *  get all galleries                      or  get all galleries that have tags apple or banana
+/* [GET] http:/localhost:3000/api/gallery  
+ *  get all galleries                     
+ *  
+ *  or you can add query params to filter out galleries
+ *  http:/localhost:3000/api/gallery?userID=64698ab60ffd0266b0c3dc89&tags=apple,banana
  */
 export const GET = async (req:Request) => {
     await connectMongoDB();
@@ -18,7 +21,11 @@ export const GET = async (req:Request) => {
         const url = new URL(req.url);
         const tags = url.searchParams.get("tags")
         const tagsArray = tags ? tags.split(',') : [];
-        const query = tags ? { tags: { $in: tagsArray } } : {};
+
+        const query = { 
+            userID: { $in: url.searchParams.get("userID") },
+            ...(tags ? { tags: { $in: tagsArray } } : {})
+        }
 
         const galleries = await Gallery.find(query).sort({"updatedAt":-1});
         return new Response(JSON.stringify(galleries), { status: 200} );
@@ -26,6 +33,7 @@ export const GET = async (req:Request) => {
         return new Response(err.message, { status: 500 });
     }
 }
+
 
 /* [POST] http:/localhost:3000/api/gallery
  *  Create a gallery 

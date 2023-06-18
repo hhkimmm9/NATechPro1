96e6bcc7b31a6646c23f3bf4bdededefe3d3f8a4
ChristyLee120 <christylee210@gmail.com>
2023-06-17 21:04:41 -0400

    Gallery page backend connected
    Gallery page backend connected done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use-client";
 export default function Home() {
   return (
     <>
@@ -10,29 +11,38 @@ export default function Home() {
           <div className="flex">
             <p className="w-1/2 p-14">
               Combine <span className="text-amber-600">multiple images</span>
-              to a <span className="text-amber-600">single</span> image
-              with any <span className="text-amber-600">background</span> of your choice
+              to a <span className="text-amber-600">single</span> image with any{" "}
+              <span className="text-amber-600">background</span> of your choice
             </p>
             <div className="w-1/2 flex justify-center">
-              <img src="https://easydrawingguides.com/wp-content/uploads/2022/07/pug-face-11.png" alt="" />
+              <img
+                src="https://easydrawingguides.com/wp-content/uploads/2022/07/pug-face-11.png"
+                alt=""
+              />
             </div>
           </div>
-          <button className="rounded-2xl text-white py-1.5 px-5 bg-amber-600 mt-6 text-sm">Try now</button>
+          <button className="rounded-2xl text-white py-1.5 px-5 bg-amber-600 mt-6 text-sm">
+            Try now
+          </button>
         </div>
       </div>
-      
+
       <div className="bg-orange-100 text-center py-6 flex flex-col gap-8">
         <h1 className="text-4xl font-semibold">4 Simple Steps</h1>
         <div className="flex justify-around">
           <div className="flex flex-col">
-            <img src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+            <img
+              src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
               alt=""
               className="w-52 h-52 rounded-full border-8 border-orange-400"
             />
-            <p className="w-48 mt-3"><span className="font-bold">Remove</span> Image Background</p>
+            <p className="w-48 mt-3">
+              <span className="font-bold">Remove</span> Image Background
+            </p>
           </div>
           <div className="flex flex-col">
-            <img src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+            <img
+              src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
               alt=""
               className="w-52 h-52 rounded-full border-8 border-orange-400"
             />
@@ -42,14 +52,19 @@ export default function Home() {
             </p>
           </div>
           <div className="flex flex-col">
-            <img src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+            <img
+              src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
               alt=""
               className="w-52 h-52 rounded-full border-8 border-orange-400"
             />
-            <p className="w-48 mt-3"><span className="font-bold">Add</span> tags to images for easy access</p>
+            <p className="w-48 mt-3">
+              <span className="font-bold">Add</span> tags to images for easy
+              access
+            </p>
           </div>
           <div className="flex flex-col">
-            <img src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+            <img
+              src="https://images.unsplash.com/photo-1543320317-15188058b450?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHVnJTIwZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80"
               alt=""
               className="w-52 h-52 rounded-full border-8 border-orange-400"
             />
@@ -61,5 +76,5 @@ export default function Home() {
         </div>
       </div>
     </>
-  )
+  );
 }

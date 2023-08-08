9c0a9e87315610d9ce42c982f11303485b9d2490
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-08-07 20:16:11 -0400

    Convert the outermost layout back to a server component.

--- src/app/components/Provider.jsx
+++ src/app/components/Provider.jsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import { CssBaseline, NextUIProvider } from "@nextui-org/react"
 
 const Provider = ({ children, session }) => {
   return (
     <SessionProvider session={session}>
-      { children }
+      <NextUIProvider>
+        { children }
+      </NextUIProvider>
     </SessionProvider>
   )
 }

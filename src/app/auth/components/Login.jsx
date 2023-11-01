f2817c83765c2ce65d8978050afa61391044631f
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-11-01 16:00:06 -0400

    Improve the styling on the Login Tab component.

--- src/app/auth/components/Login.jsx
+++ src/app/auth/components/Login.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+"use client"
+
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { signIn } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -7,11 +9,25 @@ const LoginPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
+
   const [emailInput, setEmailInput] = useState("");
   const [pwdInput, setPwdInput] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
-
   const [error, setError] = useState("");
+
+  // reactive to the remember me checkbox
+  // .... 이거 로그인 버튼 눌렸을 ㄸㅐ만..
+  useEffect(() => {
+    console.log(rememberMe)
+    if (rememberMe && emailInput) {
+      localStorage.setItem("rememberUserId", emailInput)
+    }
+
+    else {
+      if (localStorage.getItem("rememberUserId"))
+        localStorage.removeItem("rememberUserId", emailInput)
+    }
+  }, [rememberMe])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -52,21 +68,19 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col space-y-8 mt-6">
+    <>
       {/* oauth */}
-      <div className="flex flex-col gap-2 text-gray-600 w-min mx-auto">
-        <button className="py-2 pl-4 pr-8 border-2 border-gray-300 rounded-full text-center whitespace-nowrap flex gap-2 items-center">
-          <Image
-            src="/images/logos/fb-logo.png"
-            alt="Facebook Logo"
-            width={20}
-            height={20}
-            className="object-contain"
-          />
-          <span>Continue with Facebook</span>
-        </button>
+      <div className="w-min mx-auto my-8 flex flex-col gap-2 text-gray-600">
+        {/* google */}
         <button
-          className="py-2 pl-4 pr-8 border-2 border-gray-300 rounded-full text-center whitespace-nowrap flex gap-2 items-center"
+          className="
+            py-2 pr-8 pl-4
+            flex gap-2 items-center
+            border border-gray-300
+            rounded-full
+            text-center whitespace-nowrap
+          hover:bg-stone-50 hover:scale-110 duration-100
+          "
           onClick={googleSignIn}
         >
           <Image
@@ -80,42 +94,46 @@ const LoginPage = () => {
         </button>
       </div>
 
-      <div className="mx-auto text-gray-500 font-light flex flex-row items-center gap-2">
+      {/* divider */}
+      <div className="
+        mx-auto
+        flex flex-row items-center gap-2
+        font-light text-gray-500
+      ">
         <hr className="w-60" />
         <p>or</p>
         <hr className="w-60" />
       </div>
 
-      <div className="text-gray-500 font-light flex flex-col gap-2">
-        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+      {/* input fields */}
+      <div className="my-6 flex flex-col gap-2 text-gray-500 font-light">
+        <form onSubmit={handleSubmit}>
           {/* text input */}
-          <div className="flex flex-col space-y-3">
-            <div className="flex flex-col">
+          <div className="flex flex-col space-y-4">
+            {/* email */}
+            <div className="flex flex-col space-y-1">
               <label> Email </label>
               <input
                 type="text"
                 value={emailInput}
                 onChange={(e) => setEmailInput(e.target.value)}
-                className="border p-1.5"
+                className="p-1.5 border rounded-md"
               />
             </div>
-            <div className="flex flex-col">
+            {/* pwd */}
+            <div className="flex flex-col space-y-1">
               <label className="whitespace-nowrap"> Password </label>
               <input
                 type="text"
                 value={pwdInput}
                 onChange={(e) => setPwdInput(e.target.value)}
-                className="border p-1.5"
+                className="p-1.5 border rounded-md"
               />
             </div>
           </div>
 
-          {error && (
-            <span className="text-red-500 text-xs font-semibold">{error}</span>
-          )}
-
           {/* checkboxes */}
-          <div className="flex gap-2">
+          <div className="my-2 flex gap-2 items-center">
             <input
               type="checkbox"
               checked={rememberMe}
@@ -124,24 +142,30 @@ const LoginPage = () => {
             <label> Remember me </label>
           </div>
 
-          <button
-            type="submit"
-            className="bg-sky-600 w-min py-2 px-6 whitespace-nowrap rounded-full text-white hover:bg-sky-500 mx-auto"
-          >
-            {" "}
-            Log in{" "}
-          </button>
+          <div className="h-6">
+            { error && (
+              <span className="text-xs font-medium text-red-600">{ error }</span>
+            )}
+          </div>
+
+          <div className="w-min mx-auto flex flex-col">
+            <button type="submit"
+              className="
+                px-4 py-1.5
+                rounded-full bg-sky-600
+                font-medium text-white whitespace-nowrap
+                hover:scale-125 hover:font-semibold duration-100 hover:bg-sky-500
+            "> Log in </button>
+          </div>
         </form>
 
-        <div className="w-4/5 mx-auto text-center">
-          <p className="text-sm">
-            {" "}
-            This form is protected by hCaptcha and its Privacy Policy and Terms
-            of Service.{" "}
+        <div className="mt-4">
+          <p className="text-center text-sm">
+            This form is protected by hCaptcha and its Privacy Policy and Terms of Service.
           </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

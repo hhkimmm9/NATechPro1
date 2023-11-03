8f67923882bbb9e7bad506cb21661eb2f9ec427d
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-11-03 15:33:57 -0400

    Comment out some onnxruntime related configurations.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
+
+// const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
+// const webpack = require("webpack");
 
 const nextConfig = {
   experimental: {
@@ -10,41 +11,41 @@ const nextConfig = {
   },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
-    config.module.rules.push({
-      test: /\.node$/,
+    // config.module.rules.push({
+    //   test: /\.node$/,
 
-      loader: "node-loader",
-    })
+    //   loader: "node-loader",
+    // })
     // https://onnxruntime.ai/docs/tutorials/web/classify-images-nextjs-github-template.html
-    config.plugins.push(
-      new NodePolyfillPlugin(), 
-      new CopyPlugin({
-        patterns: [
-          {
-            from: './node_modules/onnxruntime-web/dist/*.wasm',
-            to: 'static/chunks/app/gallery/editor/[name][ext]',
-          },
-          // {
-          //   from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
-          //   to: 'static/chunks/app/gallery/editor',
-          // },
-          // {
-          //   from: './model',
-          //   to: 'static/chunks/app/gallery/editor',
-          // },
-        ],
-      }),
-      new webpack.DefinePlugin({
-        'process.versions': JSON.stringify(process.versions),
-        'process.env': JSON.stringify(process.env),
-      }),
-      // new webpack.ProvidePlugin({
-      //   process: "process/browser",
-      // }),
-      );
+    // config.plugins.push(
+    //   new NodePolyfillPlugin(), 
+    //   new CopyPlugin({
+    //     patterns: [
+    //       {
+    //         from: './node_modules/onnxruntime-web/dist/*.wasm',
+    //         to: 'static/chunks/app/gallery/editor/[name][ext]',
+    //       },
+    //       // {
+    //       //   from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+    //       //   to: 'static/chunks/app/gallery/editor',
+    //       // },
+    //       // {
+    //       //   from: './model',
+    //       //   to: 'static/chunks/app/gallery/editor',
+    //       // },
+    //     ],
+    //   }),
+    //   new webpack.DefinePlugin({
+    //     'process.versions': JSON.stringify(process.versions),
+    //     'process.env': JSON.stringify(process.env),
+    //   }),
+    //   // new webpack.ProvidePlugin({
+    //   //   process: "process/browser",
+    //   // }),
+    //   );
     return config;
   },
-  reactStrictMode: true,
+  // reactStrictMode: true,
   images: {
     domains: ['i.natgeofe.com', 'res.cloudinary.com'],
   },

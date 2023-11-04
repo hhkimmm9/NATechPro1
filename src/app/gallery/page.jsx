f8766bafc8ae4127b6bedaf9868685b9c3681689
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-11-03 20:20:19 -0400

    Create a new component for image cards and extract the code from the Gallery component.

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,92 +1,49 @@
-"use client";
+"use client"
 
 import React, { useState, useEffect } from "react";
-import { Text, Grid, Row, Col, Spacer, Card } from "@nextui-org/react";
-import SearchInput from "@/app/components/SearchInput"
-// import Card from "../components/Card";
-import axios from "axios";
 import { useSession } from "next-auth/react";
 
-export default function GalleryPage() {
-  const [imagesToShow, setImagesToShow] = useState([])
+import SearchInput from "@/app/components/SearchInput"
+import GalleryImages from "./GalleryImages";
 
+const GalleryPage = () => {
   const { data: session, status } = useSession({ required: true });
 
+  const [imageData, setImageDate] = useState([])
+
   useEffect(() => {
-    if (status === 'authenticated') {
-      const getImgs = async () => {
-        await axios.get("/api/gallery", {
-          headers: {
-            authorization: `Bearer ${session?.user.accessToken}`,
-          },
-        }).then(({ data }) => {
-          // console.log(data);
-          setImagesToShow(data);
-        });
-      };
-      // fetch images from the server
-      getImgs();
+    if (status === "authenticated") getImageData()
+  }, [status])
+
+  const getImageData = async () => {
+    try {
+      const res = await fetch("/api/gallery", {
+        cache: "no-store",
+        headers: {
+          authorization: `Bearer ${session?.user.accessToken}`,
+        }
+      })
+      const resJSON = await res.json()
+      setImageDate(resJSON)
+    } catch (err) {
+      // error handling here
+      console.log(err)
     }
-  }, [status]);
+  }
 
   return (
     <div className="p-5 flex flex-col h-full">
       <div>
         <text className="font-bold text-lg px-5">My Gallery</text>
-        {/* <SearchInput /> */}
+        <SearchInput />
       </div>
       <div>
         <text className="px-5 font-semibold">Sort By: </text>
       </div>
-      <div className="p-5 flex bg-blue-900/10 rounded-lg">
-        {imagesToShow && (
-          <Grid.Container gap={2} wrap="wrap" justify="flex-start">
-            {imagesToShow.map((item, index) => (
-              <Grid xs={6} sm={3} key={index}>
-                <Card isPressable borderWeight="light">
-                  <Card.Body css={{ p: 0 }}>
-                    <Card.Image
-                      src={item.image}
-                      // { imagesToShow?.map(item =>  {item.image} ) }
-                      objectFit="cover"
-                      width={240}
-                      height={200}
-                      alt={item.name}
-                    />
-                  </Card.Body>
-                  <Card.Footer css={{ justifyItems: "flex-start" }}>
-                    <Row wrap="wrap" justify="space-between" align="center">
-                      <Text b>{item.name}</Text>
-                      <Col>
-                        <Text
-                          css={{
-                            color: "$accents7",
-                            fontWeight: "$semibold",
-                            fontSize: "$sm",
-                          }}
-                        >
-                          Tags: {item.tags}
-                        </Text>
-                      </Col>
-                      <Col>
-                        <Text
-                          css={{
-                            color: "$accents7",
-                            fontWeight: "$semibold",
-                            fontSize: "$sm",
-                          }}
-                        >
-                          Date: no date on the images
-                        </Text>
-                      </Col>
-                    </Row>
-                  </Card.Footer>
-                </Card>
-              </Grid>
-            ))}
-          </Grid.Container>
-        )}
-      </div>
+
+      <GalleryImages imageData={imageData} />
     </div>
   );
 }
+
+export default GalleryPage

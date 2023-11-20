6efca1c4200ca7a4559040e62193660d5ed4cebf
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2023-11-20 16:12:25 -0500

    CP - trying to figure out how to get a response from the image embedding server.

--- src/app/gallery/editor/page.tsx
+++ src/app/gallery/editor/page.tsx
@@ -1,49 +1,44 @@
 'use client'
 
 import React, { useContext, useState, useEffect, useCallback, useRef } from 'react'
-import Image from 'next/image'
 import Script from 'next/script'
 import AppContext from "@/app/components/hooks/createContext"
-import { InferenceSession, Tensor } from "onnxruntime-web"
-const ort = require("onnxruntime-web")
-import npyjs from "npyjs"
+
 import { useDropzone } from 'react-dropzone'
 import Cropper, { ReactCropperElement } from "react-cropper"
 import "cropperjs/dist/cropper.css";
 
-export default function EditorPage() {
+const ort = require("onnxruntime-web")
+import npyjs from "npyjs"
+import { InferenceSession, Tensor } from "onnxruntime-web"
+
+const EditorPage: React.FC = () => {
   const {
     clicks: [clicks],
     image: [, setImage],
     maskImg: [, setMaskImg],
   } = useContext(AppContext)
 
-  const cropperRef = useRef(null)
-  const onCrop = () => {
-    const cropper = cropperRef.current?.cropper
-    // console.log(cropper.getCroppedCanvas().toDataURL())
-    console.log(cropper.getCroppedCanvas())
-
-    const formData = new FormData()
-    formData.append('croppedImage', cropper.getCroppedCanvas().toBlob())
-  }
-
-  // const IMAGE_PATH = "/assets/data/truck.jpg";
-  
   const [imageUrl, setImageUrl] = useState('')
   const [imageSelected, setImageSelected] = useState(false)
   const [model, setModel] = useState(null) // ONNX model
   const [tensor, setTensor] = useState(null) // Image embedding tensor
 
-  useEffect(() => {
-    // samInit()
-  }, [])
+  const cropperRef = useRef<ReactCropperElement>(null)
+  const onCrop = (event: any) => {
+    // console.log(event.detail.x);
+    // console.log(event.detail.y);
+    // console.log(event.detail.width);
+    // console.log(event.detail.height);
+    // console.log(event.detail.rotate);
+    // console.log(event.detail.scaleX);
+    // console.log(event.detail.scaleY);
 
-  // Initialize the ONNX model
-  const samInit = async () => {
-    const MODEL_DIR = "/assets/models/sam_onnx_quantized.onnx"
-    const IMAGE_EMBEDDING = "/assets/data/truck_embedding.npy"
-  };
+    const cropper = cropperRef.current?.cropper
+
+    // console.log(cropper?.getCroppedCanvas().toDataURL('image/jpeg'))
+    // console.log(cropper?.getCroppedCanvas())
+  }
 
   // handling the drag and drop box.
   const MyDropzone = () => {
@@ -53,7 +48,7 @@ export default function EditorPage() {
 
       let url = window.URL.createObjectURL(acceptedFiles[0])
 
-      // change image in front page
+      // change image in the page
       setImageUrl(url)
     }, [])
 
@@ -95,44 +90,69 @@ export default function EditorPage() {
     }
   }
   
-  const changeImageEvent = async (imageFile: any) => {
-    // send image to server
-    let formData = new FormData()
-    formData.append("file", imageFile, "image.jpg")
-    await fetch("http://localhost:8888/image-embedding", {
-      method: 'POST',
-      body: formData,
-      headers: {
-        accept: "application/json",
-      }
-    }).then((response) => response.json())
-      .then((data) => {
+  const submitImage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
 
-        // 
-        console.log(data)
-        let image_embedding = data.image_embedding
-        console.log(image_embedding)
+    const cropper = cropperRef.current?.cropper
+    const formData = new FormData()
 
-        const binaryString = atob(image_embedding);
-        // Create a DataView to read the binary data as float32 values
-        const dataView = new DataView(new ArrayBuffer(binaryString.length));
-        for (let i = 0; i < binaryString.length; i++) {
-            dataView.setUint8(i, binaryString.charCodeAt(i));
+    cropper?.getCroppedCanvas({
+      width: 288,
+      height: 162,
+    });
+
+    cropper?.getCroppedCanvas({
+      minWidth: 256,
+      minHeight: 256,
+      maxWidth: 4096,
+      maxHeight: 4096,
+    })
+
+    await cropper?.getCroppedCanvas().toBlob((blob: any) => {
+      let imageFile = new File([blob], "image.jpg", { type: "image/jpeg" })
+      formData.append("file", imageFile, "image.jpg")
+    }, "image/jpeg")
+    
+    try {
+      // send image to server
+      const response = await fetch("http://0.0.0.0:8888/image-embedding", {
+        method: "POST",
+        body: formData,
+        headers: {
+          // 'Content-Type': 'multipart/form-data'
+          accept: "application/json"
         }
-        
-        // Read the float32 values from the DataView
-        // const float32Array = new Float32Array(dataView.buffer);
-        // tensor = new ort.Tensor("float32", float32Array, data.image_embedding_shape);
-
-        // ----------------------------------------------------------------
-
-        // Load the Segment Anything pre-computed embedding
-        Promise.resolve(loadNpyTensor(image_embedding, "float32")).then(
-          (embedding) => setTensor(embedding)
-        );
-      }).catch((e) => {
-        alert('사용 할 수 없는 이미지 입니다.')
       })
+  
+      const result = await response.json()
+
+      console.log('result: ', result.data)
+      // let image_embedding = result.data.image_embedding
+      // console.log(image_embedding)
+
+      // const binaryString = atob(image_embedding);
+      // // Create a DataView to read the binary data as float32 values
+      // const dataView = new DataView(new ArrayBuffer(binaryString.length));
+      // for (let i = 0; i < binaryString.length; i++) {
+      //     dataView.setUint8(i, binaryString.charCodeAt(i));
+      // }
+      
+      // Read the float32 values from the DataView
+      // const float32Array = new Float32Array(dataView.buffer);
+      // tensor = new ort.Tensor("float32", float32Array, data.image_embedding_shape);
+
+      // ----------------------------------------------------------------
+
+      // Load the Segment Anything pre-computed embedding
+      // Promise.resolve(loadNpyTensor(image_embedding, "float32")).then(
+      //   (embedding) => setTensor(embedding)
+      // );
+    }
+
+    catch(error: any) {
+      console.log(error.message);
+      alert('사용 할 수 없는 이미지 입니다.')
+    }
   }
 
   // Decode a Numpy file into a tensor. 
@@ -165,20 +185,21 @@ export default function EditorPage() {
               ref={cropperRef}
             />
 
-            <div className='mt-8 flex gap-8 justify-center'>
+            <form onSubmit={(e) => submitImage(e)} className='mt-8 flex gap-8 justify-center'>
               <button onClick={() => { setImageUrl(''); setImageSelected(false); }}
                 className='
                   px-2 py-1 border rounded-xl hover:bg-stone-50
-              '>Cancel</button>
+              '> Cancel </button>
 
-              <button onClick={() => {}}
-                className='
+              <button type="submit" className='
                   px-2 py-1 border rounded-xl hover:bg-blue-100
-              '>Submit</button>
-            </div>
+              '> Submit </button>
+            </form>
           </>
         )}
       </div>
     </>
   )
 }
+
+export default EditorPage
